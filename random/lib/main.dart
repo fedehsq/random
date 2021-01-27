449e37f8e995defd069dc97bfaaae662e8f1029a
Federico Bernacca <60901397+fedehsq@users.noreply.github.com>
2021-01-27 20:29:13 +0100

    Update main.dart

--- random/lib/main.dart
+++ random/lib/main.dart
@@ -12,19 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Filler(),
@@ -98,7 +86,6 @@ class _RandomizerState extends State<Randomizer> {
                     shown = (shown[2] == '0') ? shown.replaceRange(2, 3, '') : shown;
                     widget.doubles.removeAt(i);
                   }
-                  // remove item from list
                 }),
                 child: Text('Generate', style: TextStyle(color: Colors.white),),
             color: Colors.blue,)
